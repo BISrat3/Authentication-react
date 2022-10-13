cddb90c3eef3933d0eab296241e72515da28bcf7
bisrat <bisrat.amtataw@gmail.com>
2022-10-13 14:32:07 -0400

    clean up function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,33 +11,58 @@ export default function Login(props) {
     const [passwordIsValid, setPasswordIsValid] =useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
+    useEffect (() => {
+        console.log("Effect Running")
+
+         return () =>{
+            console.log("Effect Cleanup")
+         }
+    }, [])
+
     // to handle side effect - are http request. 
     // useEffects are executed in response to something, it could be loading anything that is related to action. 
-    useEffect(()=>{
-        const identifier = setTimeout(() =>{
-        console.log("Checking form validity!")
-        setFormIsValid(
-            enteredEmail.includes('@') && enteredPassword.trim().length >6)
-        }, 500);
+    // useEffect(()=>{
+    //     const identifier = setTimeout(() =>{
+    //     console.log("Checking form validity!")
+    //     setFormIsValid(
+    //         enteredEmail.includes('@') && enteredPassword.trim().length >6)
+    //     }, 500);
         
 
-        return () =>{
-            console.log("Running Clean up")
-            clearTimeout(identifier)
-        }
-    }, [enteredEmail, enteredPassword])
+    //     return () =>{
+    //         console.log("Running Clean up")
+    //         clearTimeout(identifier)
+    //     }
+    // }, [enteredEmail, enteredPassword])
 
     const emailChangeHandler = (event) =>{
         setEnteredEmail(event.target.value);
+
+        setFormIsValid(
+           event.target.value.includes('@') && enteredPassword.trim().length >6
+        )
     }
 
     const passwordChangeHandler = (event) => {
         setEnteredPassword (event.target.value)
+
+        setFormIsValid(
+        enteredEmail.includes('@') && event.target.value.trim().length >6)
     }
 
     const validateEmailHandler = () =>{
-        setFormIsValid (enteredEmail.includes('@'))
+        setEmailIsValid (enteredEmail.includes('@'))
     }
+
+    const validatePasswordHandler = () =>{
+        setPasswordIsValid (enteredPassword.trim().length >6)
+    }
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+        props.onLogin(enteredEmail, enteredPassword)
+    }
+
   return (
     <div>Login</div>
   )
