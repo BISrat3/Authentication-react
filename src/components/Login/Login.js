75238a59f105bafaad587dea3ec4ac27157fc697
bisrat <bisrat.amtataw@gmail.com>
2022-10-17 15:23:10 -0400

    useimparative handle the login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect, useReducer, useContext} from 'react'
+import React,{useState, useEffect, useReducer, useContext, useRef} from 'react'
 
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
@@ -48,6 +48,9 @@ export default function Login(props) {
         value :'',
         isValid: undefined,
     })
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
 
     useEffect (() => {
         console.log("Effect Running")
@@ -108,13 +111,22 @@ export default function Login(props) {
 
     const submitHandler = (event) =>{
         event.preventDefault();
-        authCtx.onLogin(emailState.value, passwordState.value)
+        if (formIsValid){
+            authCtx.onLogin(emailState.value, passwordState.value)
+        }
+        else if (!emailIsValid){
+            emailInputRef.current.focus();
+        }
+        else{
+            passwordInputRef.current.focus();
+        }
     }
 
   return (
     <Card className={classes.login}>
         <form onSubmit={submitHandler}>
             <Input 
+                ref= {emailInputRef}
                 id='email' 
                 label='E-Mail' 
                 type='email' 
@@ -123,7 +135,17 @@ export default function Login(props) {
                 onBlur={validateEmailHandler}
                 onChange={emailChangeHandler}
             />
-            <div
+            <Input 
+                ref= {passwordInputRef}
+                id='password' 
+                label='Password' 
+                type='password' 
+                isValid={passwordIsValid} 
+                value={passwordState.value}
+                onBlur={validatePasswordHandler}
+                onChange={passwordChangeHandler}
+            />
+            {/* <div
                 className={`${classes.control} ${
                     passwordState.isValid === false ? classes.invalid: ''
                 }`}>
@@ -135,9 +157,10 @@ export default function Login(props) {
                     onChange={passwordChangeHandler}
                     onBlur={validatePasswordHandler}
                     />
-            </div>
+            </div> */}
             <div className={classes.actions}>
-                <Button type="submit" className={classes.btn} disabled={!formIsValid}> 
+                {/* <Button type="submit" className={classes.btn} disabled={!formIsValid}>  */}
+                <Button type="submit" className={classes.btn}>
                     Login
                 </Button>
             </div>
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, {useRef, useEffect, useImperativeHandle}from 'react'
 
 import classes from './input.module.css' 
 
-export default function Input(props) {
+ const Input = React.forwardRef ((props, ref) => {
+    const inputRef = useRef();
+
+    // useEffect (() =>{
+    //     inputRef.current.focus();
+    // }, [])
+
+    const activate = () =>{
+        inputRef.current.focus();
+    }
+
+    // this is the translation between inside of the component and outside
+    useImperativeHandle (ref, () =>{
+        return {
+            focus: activate
+        }
+    })
+
   return (
     <div className={`${classes.control} ${
         props.isValid === false ? classes.invalid: ''
     }`}>
         <label htmlFor={props.id}>{props.label}</label>
         <input 
+            ref={inputRef}
             type={props.type}
             id={props.id}
             value={props.value}
@@ -17,4 +35,6 @@ export default function Input(props) {
             />
     </div>
   )
-}
+})
+
+export default Input
